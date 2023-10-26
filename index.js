239df363ef54c09cd9e01e9f594a8e8e62fdebc1
hadil <hihijazi@gmail.com>
2023-10-26 16:25:16 -0400

    save changes in index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+    return cats;
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Garfield")
+    return cats;
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo")
+    return cats;
+}
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;   
+}
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;   
+}
+function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+}
